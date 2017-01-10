corrected gradient computation for L2, xentropy to do

dautoencoder.py
@@ -32,7 +32,7 @@ class AutoEncoder(classae.GenericAutoEncoder):
 			else:
 				corruption_lvl = corruption
 				
-			layer = network.Layer(architecture[ii], layers_activations[ii], corruption_lvl)
+			layer = network.Layer(architecture[ii], layers_activations[ii], corruption_lvl, debug=self.debug)
 			layer.regularisation = regularisation
 			layer.train(data, mini_batch=mini_batch, iterations=iterations, **kwargs)
 			logger.info("Finished with pre-training layer {} ({}/{})...".format(layer.weights.shape, ii + 1, len(architecture)))
@@ -44,6 +44,59 @@ class AutoEncoder(classae.GenericAutoEncoder):
 		self.is_pretrained = True
 		self.set_autoencoder(layers)
 		logger.info("Finished with pre-training...")
+		
+	def gd(self, data, iterations, cost_fct, learning_rate=0.1, regularisation=0, corruption=None, **kwargs):
+		"""
+		Pre-training of a dA layer with cross-entropy (hard coded -- at least for now)
+		
+		:param data: The data to be used
+		:param iterations: the number of iterations
+		:param cost_fct: what cost function to use
+		:param mini_batch: number of samples to use per batch
+		:param regularisation: the multipler of the L2 regularisation
+		:param method: which method of `scipy.optimize.minimize` to use
+		:pram weights: the standard deviation of the zero-centred weights (biases are initialised to 0)
+		
+		all remaining `kwargs` are passed to `scipy.optimize.minimize`.
+		"""
+		for it in range(iterations):
+			# Copy the weights and biases into a state vector theta ################################
+			weights = []
+			biases = []
+			for jj in range(self.mid * 2):
+				weights.append(copy.copy(self.layers[jj].weights))
+				biases.append(self.layers[jj].biases) 
+				
+			theta, indices, weights_shape, biases_shape = self._roll(weights, biases)
+			
+			# Selecting the cost function ##########################################################
+			if cost_fct == 'cross-entropy':
+				cost, grad = self.xentropy_cost(theta, data, corruption=corruption)
+			elif cost_fct == 'L2':
+				cost, grad = self.l2_cost(theta, data, corruption=corruption)
+			else:
+				raise ValueError("Cost function {} unknown".format(cost_fct))
+			########################################################################################
+			
+			logger.info("{}it: {:0.6f}".format(it, cost))
+
+			# Unroll the state vector and saves it to self #########################################	
+			for jj in range(self.mid * 2):
+					
+				w, b = self._unroll(grad, jj, indices, weights_shape, biases_shape)
+				
+				self.layers[jj].weights -= learning_rate * w
+				self.layers[jj].biases -= learning_rate * b
+			# Copy the weights and biases into a state vector theta ################################
+			weights = []
+			biases = []
+			for jj in range(self.mid * 2):
+				weights.append(copy.copy(self.layers[jj].weights))
+				biases.append(self.layers[jj].biases) 
+				
+			theta, indices, weights_shape, biases_shape = self._roll(weights, biases)
+			self._log()
+		return
 		
 	def fine_tune(self, data, iterations, cost_fct, regularisation=0.0, mini_batch=0, 
 					method='L-BFGS-B', corruption=None, **kwargs):
@@ -66,6 +119,22 @@ class AutoEncoder(classae.GenericAutoEncoder):
 		self.mini_batch = mini_batch
 		self.Ndata = self.layers[0].Ndata 
 		time_start = datetime.now()
+
+		# Copy the weights and biases into a state vector theta ###################################
+		weights = []
+		biases = []
+		for jj in range(self.mid * 2):
+			weights.append(copy.copy(self.layers[jj].weights))
+			biases.append(self.layers[jj].biases) 
+			
+		theta, indices, weights_shape, biases_shape = self._roll(weights, biases)
+		self.weights_shape = weights_shape
+		self.biases_shape = biases_shape
+		self.indices = indices
+
+		if method == "gd":
+			self.gd(data, iterations, cost_fct, regularisation=regularisation, corruption=corruption, **kwargs)
+			return
 
 		###########################################################################################
 		# Optimisation of the weights and biases according to the cost function
@@ -150,7 +219,8 @@ class AutoEncoder(classae.GenericAutoEncoder):
 			self.layers[jj].weights = w
 			self.layers[jj].biases = b
 
-		m = data.shape[1]
+		m, Npix = data.shape
+		Npix, m = data.shape
 
 		# Forward pass
 		if corruption is not None:
@@ -217,7 +287,7 @@ class AutoEncoder(classae.GenericAutoEncoder):
 			self.layers[jj].biases = b
 	
 		# Number of training examples
-		m = data.shape[1]
+		m, _ = data.shape
 
 		# Forward pass
 		if corruption is not None:
@@ -241,11 +311,10 @@ class AutoEncoder(classae.GenericAutoEncoder):
 			if jj < self.mid * 2 - 1:
 
 				delta = (self.layers[jj+1].weights.dot(delta.T)).T
-				delta = np.array(delta)
 			
 			delta *= self.layers[jj].activation_fct_prime(self.layers[jj].activation)
 			
-			grad_w = (((self.layers[jj].input.T).dot(delta)) + self.regularisation * self.layers[jj].weights )/ m
+			grad_w = (((self.layers[jj].input.T).dot(delta))) / m + self.regularisation * self.layers[jj].weights
 			grad_b = np.mean(delta, axis=0)
 
 			wgrad.append(grad_w)

demo/SAE_cifar10.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='PID %(process)06d | %(asctime)s | %(levelname)s: %(n
 
 # Loading the data and pre-processing
 N_train = 5000
-N_test = 1000
+N_test = 5000
 
 images = u.load_images("cifar-10-batches-py", N_train+N_test)
 
@@ -22,20 +22,20 @@ images_test = images[N_train:N_train+N_test]
 # Preparing the SAE
 dA = pylae.dA.AutoEncoder("sae_cifar")
 
-architecture = [128]#[512, 64]
-layers_activation = ["SIGMOID"]#, "LINEAR"]
+architecture = [128]#512, 256, 128]
+layers_activation = ["SIGMOID"]#, "SIGMOID", "LINEAR"]
 cost_fct = 'L2'
 
 # Define what training we should do
-do_pre_train = False
+do_pre_train = True
 do_train = True
-iters = 5000
+iters = 500
 
 corruption = None
 
 # Layer pre-training
 if do_pre_train:
-	dA.pre_train(images_train, architecture, layers_activation, iterations=iters, mini_batch=0, cost_fct=cost_fct, corruption=corruption)
+	dA.pre_train(images_train, architecture, layers_activation, iterations=iters, mini_batch=0, cost_fct=cost_fct, corruption=corruption)#, method="gd")
 	dA.save()
 else:
 	dA = pylae.utils.readpickle(os.path.join(dA.filepath, 'ae.pkl'))
@@ -73,6 +73,7 @@ images_pca = pca.inverse_transform(pca_enc)
 images_pca, _, _ = pylae.processing.normalise(images_pca) # PCA IS NOT NORMALISED!
 
 # Print a few metrics
+logging.info('Reminder, corruption is {}'.format(corruption))
 logging.info('** cross-entropy **')
 logging.info('AE cost: {:1.3f}'.format(pylae.utils.cross_entropy(images_test, images_sae)))
 logging.info('PCA cost: {:1.3f}'.format(pylae.utils.cross_entropy(images_test, images_pca)))
@@ -85,6 +86,10 @@ logging.info('** explained variance **')
 logging.info('AE cost: {:1.2f}'.format(metrics.explained_variance_score(images_test, images_sae)))
 logging.info('PCA cost: {:1.2f}'.format(metrics.explained_variance_score(images_test, images_pca)))
 
+logging.info('** Residues **')
+logging.info('AE cost: {:1.2f}'.format(np.abs(images_test - images_sae).mean()))
+logging.info('PCA cost: {:1.2f}'.format(np.abs(images_test - images_pca).mean()))
+
 # Now show the reconstructed images
 size = int(np.sqrt(np.shape(images_test)[1]))
 # Show the original image, the reconstruction and the residues for the first 10 cases
@@ -94,22 +99,23 @@ for ii in np.random.choice(range(N_test), size=10, replace=False):
 	recont = images_pca[ii].reshape(size,size)
 
 	plt.figure(figsize=(9,6))
+	opts = {"vmin":0., "vmax":1., "cmap":"Greys"}
 	
 	plt.subplot(2, 3, 1)
-	plt.imshow((img), interpolation="nearest")
+	plt.imshow((img), interpolation="nearest", **opts)
 	
 	plt.subplot(2, 3, 2)
-	plt.title("Gradient Desc.")
-	plt.imshow((recon), interpolation="nearest")
+	plt.title("AE")
+	plt.imshow((recon), interpolation="nearest", **opts)
 	plt.subplot(2, 3, 3)
-	plt.imshow((img - recon), interpolation="nearest")
-	plt.title("Gradient Desc. residues")
+	plt.imshow(np.abs(img - recon), interpolation="nearest", **opts)
+	plt.title("AE residues")
 		
 	plt.subplot(2, 3, 5)
-	plt.imshow((recont), interpolation="nearest")
+	plt.imshow((recont), interpolation="nearest", **opts)
 	plt.title("PCA")
 	plt.subplot(2, 3, 6)
-	plt.imshow((img - recont), interpolation="nearest")
+	plt.imshow(np.abs(img - recont), interpolation="nearest", **opts)
 	plt.title("PCA residues")
 
 plt.show()

demo/SAE_mnist.py
@@ -12,7 +12,7 @@ import logging
 logging.basicConfig(format='PID %(process)06d | %(asctime)s | %(levelname)s: %(name)s(%(funcName)s): %(message)s',level=logging.INFO)
 
 # Loading the data and pre-processing
-N_train = 50000
+N_train = 5000
 N_test = 1500
 
 images = utils_mnist.load_MNIST_images('mnist-data/train-images-idx3-ubyte')
@@ -24,16 +24,16 @@ images_train = images[ids_train]
 images_test = images[ids_test]
 
 # Preparing the SAE
-dA = pylae.dA.AutoEncoder("sae_mnist")
+dA = pylae.dA.AutoEncoder("sae_mnist_single")
 
-architecture = [128, 64, 8]
-layers_activation = ["SIGMOID", "SIGMOID", "SIGMOID"]
+architecture = [8]#128, 64, 8]
+layers_activation = ["SIGMOID"]#, "SIGMOID", "SIGMOID"]
 cost_fct = 'cross-entropy'
 
 # Define what training we should do
-do_pre_train = True
-do_train = True
-iters = 5000
+do_pre_train = False
+do_train = False
+iters = 500
 
 # Layer pre-training
 if do_pre_train:
@@ -87,6 +87,10 @@ logging.info('** explained variance **')
 logging.info('AE cost: {:1.2f}'.format(metrics.explained_variance_score(images_test, images_sae)))
 logging.info('PCA cost: {:1.2f}'.format(metrics.explained_variance_score(images_test, images_pca)))
 
+logging.info('** Residues **')
+logging.info('AE cost: {:1.2f}'.format(np.abs(images_test - images_sae).mean()))
+logging.info('PCA cost: {:1.2f}'.format(np.abs(images_test - images_pca).mean()))
+
 # Now show the reconstructed images
 size = int(np.sqrt(np.shape(images_test)[1]))
 # Show the original image, the reconstruction and the residues for the first 10 cases
@@ -96,22 +100,23 @@ for ii in np.random.choice(range(N_test), size=10, replace=False):
 	recont = images_pca[ii].reshape(size,size)
 
 	plt.figure(figsize=(9,6))
+	opts = {"vmin":0., "vmax":1., "cmap":"Greys"}
 	
 	plt.subplot(2, 3, 1)
-	plt.imshow((img), interpolation="nearest")
+	plt.imshow((img), interpolation="nearest", **opts)
 	
 	plt.subplot(2, 3, 2)
-	plt.title("Gradient Desc.")
-	plt.imshow((recon), interpolation="nearest")
+	plt.title("AE")
+	plt.imshow((recon), interpolation="nearest", **opts)
 	plt.subplot(2, 3, 3)
-	plt.imshow((img - recon), interpolation="nearest")
-	plt.title("Gradient Desc. residues")
+	plt.imshow(np.abs(img - recon), interpolation="nearest", **opts)
+	plt.title("AE residues")
 		
 	plt.subplot(2, 3, 5)
-	plt.imshow((recont), interpolation="nearest")
+	plt.imshow((recont), interpolation="nearest", **opts)
 	plt.title("PCA")
 	plt.subplot(2, 3, 6)
-	plt.imshow((img - recont), interpolation="nearest")
+	plt.imshow(np.abs(img - recont), interpolation="nearest", **opts)
 	plt.title("PCA residues")
 
 plt.show()

layers/dA_layer.py
@@ -11,7 +11,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Layer(layer.AE_layer):
-	def __init__(self, hidden_nodes, activation, corruption):
+	def __init__(self, hidden_nodes, activation, corruption, debug):
 		"""
 		:param hidden_nodes: Number of neurons in layer
 		:param activation: Activation function for the layer
@@ -20,18 +20,26 @@ class Layer(layer.AE_layer):
 			reached to probe the rmsd behaviour. 
 		:param early_stop: if True, stops the iterations when there is no improvements anymore, 
 			makes probes the `max_epoch_without_improvement` following iterations before stopping.
+		:param debug: make a slow but useful gradient checking operations.
 		"""
 		self.hidden_nodes = hidden_nodes
 		self.activation_name = activation
 		self.train_history = []
 		self.corruption = corruption
+		self.debug = debug
+		self.it = 0
 		
 		self.activation_fct = eval("act.{}".format(self.activation_name.lower()))
 		self.activation_fct_prime = eval("act.{}_prime".format(self.activation_name.lower()))
-	
-	def xentropy_cost(self, theta, data):
 		
-		m = data.shape[1]
+	def xentropy_cost(self, theta, data):
+		"""
+		Computes the L2 cost and gradient
+		http://neuralnetworksanddeeplearning.com/chap3.html for details
+		"""
+		
+		m, Npix = data.shape
+		Npix, m = data.shape
 
 		# Unroll theta
 		self.weights, self.inverse_biases, self.biases = self._unroll(theta)
@@ -62,23 +70,49 @@ class Layer(layer.AE_layer):
 		grad = self._roll(dEdw, dEdvb, dEdhb)
 
 		# Computes the cross-entropy
-		self.current_cost_value = utils.cross_entropy(data, ch)
+		self.current_cost_value = utils.cross_entropy(data, ch) / Npix
+		
+
+		if self.debug and self.it > 2:
+			epsilon = 1e-4
+			
+			print "-id-|--grad---|num-grad-|-delta-|-mult--|status"
+			print "----|---------|---------|-------|-------|------"
+			
+			debug_res = True
+			for i in range(np.size(theta)):
+				basis_vectors = np.zeros_like(theta)
+				basis_vectors[i] = 1
+				tetha_p = theta + epsilon * basis_vectors
+				tetha_m = theta - epsilon * basis_vectors
+				
+				self.weights, self.inverse_biases, self.biases = self._unroll(tetha_p)
+				h = self.round_feedforward(data)
+				cost_p = utils.cross_entropy(data, h) / Npix
+				
+				self.weights, self.inverse_biases, self.biases = self._unroll(tetha_m)
+				h = self.round_feedforward(data)
+				cost_m = utils.cross_entropy(data, h) / Npix
+				
+				num_grad = 0.5 * (cost_p - cost_m) / epsilon
+				
+				status = np.abs(num_grad - grad[i]) < epsilon
+				
+				if not status:
+					debug_res = False
+				print "{0:04d}|{1:+0.6f}|{2:+0.6f}|{3:0.5f}|{4:0.5f}|{5}".format(i, grad[i], num_grad, np.abs(num_grad - grad[i]), num_grad / grad[i], status)
+			
+			print 'Final status of debug:', debug_res
 		
 		# Returns the gradient as a vector.
 		return self.current_cost_value, grad
 	
 	def l2_cost(self, theta, data):
-		"""
-		Computes the L2 cost and gradient
-		http://neuralnetworksanddeeplearning.com/chap3.html for details
-		"""
 
-		m = data.shape[1]
-			
+		m, Npix = data.shape
 
 		# Unroll theta
 		self.weights, self.inverse_biases, self.biases = self._unroll(theta)
-
 		# Forward passes
 		
 		if self.corruption is not None:
@@ -91,24 +125,74 @@ class Layer(layer.AE_layer):
 		if self.corruption is None:
 			ch = h
 			
+		# Slow but right way
+		"""
+		prime_last = self.activation_fct_prime(activation_last)
+		prime = self.activation_fct_prime(self.activation)
+		for i in range(m):
+			deltal = -(cdata[i] - cdata[i]) * prime_last[i]
+			
+			print deltal.shape
+			exit()
+		
+		deltal = (ch - data) * prime
+		dEdvb = np.mean(deltal, axis=0)
+		
+
+		deltal = np.dot(deltal, self.weights) * prime
+		dEdhb = np.mean(deltal, axis=0)
+		
+		dEdw = (data.T.dot(deltal)) / m + self.regularisation * self.weights
+		grad = self._roll(dEdw, dEdvb, dEdhb)
+		
+		
+		self.current_cost_value = np.sum((h - data) ** 2) / (2 * m) + ((self.regularisation / 2) * np.abs(self.weights)**2).sum()
+		self.current_cost_value /= Npix
+		"""	
 		# Back-propagation
 		prime = self.activation_fct_prime(activation_last)
-		deltaL = (ch - data) * prime
-		dEdvb = np.mean(deltaL, axis=0)
+		deltal = (ch - data) * prime
+		dEdvb = np.mean(deltal, axis=0)
 		
 		prime = self.activation_fct_prime(self.activation)
+		deltal = np.dot(deltal, self.weights) * prime
+		dEdhb = np.mean(deltal, axis=0)
 		
-		deltal = np.multiply((np.dot(deltaL, self.weights)), prime)
-		deltal = np.array(deltal)
-		dEdhb = np.mean(deltal, axis=0) 
-		
-		dEdw = (((deltal.T).dot(data)).T + self.regularisation * self.weights) / m
-
+		dEdw = (ch.T.dot(deltal)) / m + self.regularisation * self.weights
 		grad = self._roll(dEdw, dEdvb, dEdhb)
 		
 		self.current_cost_value = np.sum((h - data) ** 2) / (2 * m) + ((self.regularisation / 2) * np.abs(self.weights)**2).sum()
-					
-		
+
+		if self.debug and self.it > 2:
+			epsilon = 1e-4
+			
+			print "-id-|--grad---|num-grad-|-delta-|-mult--|status"
+			print "----|---------|---------|-------|-------|------"
+			
+			debug_res = True
+			for i in range(np.size(theta)):
+				basis_vectors = np.zeros_like(theta)
+				basis_vectors[i] = 1
+				tetha_p = theta + epsilon * basis_vectors
+				tetha_m = theta - epsilon * basis_vectors
+				
+				self.weights, self.inverse_biases, self.biases = self._unroll(tetha_p)
+				h = self.round_feedforward(data)
+				cost_p = np.sum((h - data) ** 2) / (2 * m)
+				
+				self.weights, self.inverse_biases, self.biases = self._unroll(tetha_m)
+				h = self.round_feedforward(data)
+				cost_m = np.sum((h - data) ** 2) / (2 * m)
+				
+				num_grad = 0.5 * (cost_p - cost_m) / epsilon
+				
+				status = np.abs(num_grad - grad[i]) < epsilon
+				
+				if not status:
+					debug_res = False
+				print "{0:04d}|{1:+0.6f}|{2:+0.6f}|{3:0.5f}|{4:0.5f}|{5}".format(i, grad[i], num_grad, np.abs(num_grad - grad[i]), num_grad / grad[i], status)
+			
+			print 'Final status of debug:', debug_res
 		
 		# Returns the gradient as a vector.
 		return self.current_cost_value, grad
@@ -118,6 +202,46 @@ class Layer(layer.AE_layer):
 		Used as a callback function for the minization algorithm
 		"""
 		self.train_history.append(self.current_cost_value)
+		self.it += 1
+		
+	def gd(self, data, iterations, cost_fct, learning_rate=0.1, regularisation=0, **kwargs):
+		"""
+		Pre-training of a dA layer with cross-entropy (hard coded -- at least for now)
+		
+		:param data: The data to be used
+		:param iterations: the number of iterations
+		:param cost_fct: what cost function to use
+		:param mini_batch: number of samples to use per batch
+		:param regularisation: the multipler of the L2 regularisation
+		:param method: which method of `scipy.optimize.minimize` to use
+		:pram weights: the standard deviation of the zero-centred weights (biases are initialised to 0)
+		
+		all remaining `kwargs` are passed to `scipy.optimize.minimize`.
+		"""
+		
+		self.Ndata, numdims = np.shape(data)
+		
+		time_start = datetime.now()
+
+		for it in range(self.iterations):
+			theta = self._roll(self.weights, self.inverse_biases, self.biases)
+			# Selecting the cost function ##########################################################
+			if cost_fct == 'cross-entropy':
+				cost, grad = self.xentropy_cost(theta, data)
+			elif cost_fct == 'L2':
+				cost, grad = self.l2_cost(theta, data)
+			else:
+				raise ValueError("Cost function {} unknown".format(cost_fct))
+			########################################################################################
+			logger.info("{}it: {:0.6f}".format(it, cost))
+			dweights, dinverse_biases, dbiases = self._unroll(grad)
+			self.weights -= dweights * learning_rate
+			self.inverse_biases -= dinverse_biases * learning_rate
+			self.biases -= dbiases * learning_rate
+			theta = self._roll(self.weights, self.inverse_biases, self.biases)
+			self._log()
+		now = datetime.now()
+		logger.info("Pre-training of layer {} done in {}...".format(self.weights.shape, (now - time_start)))
 		
 	
 	def train(self, data, iterations,  cost_fct, mini_batch=0, regularisation=0, method='L-BFGS-B', weight=0.1, **kwargs):
@@ -161,7 +285,13 @@ class Layer(layer.AE_layer):
 		
 		self.inverse_biases = np.zeros(numdims)
 		self.biases = np.zeros(self.hidden_nodes)
+		
 		time_start = datetime.now()
+		
+		if method == "gd":
+			# This is a bit special. It is mostly for debug purposes.
+			self.gd(data, iterations, cost_fct, regularisation=regularisation, **kwargs)
+			return
 	
 		###########################################################################################
 		options_ = {'maxiter': self.iterations, 'disp': True}